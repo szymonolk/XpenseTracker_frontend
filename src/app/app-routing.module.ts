a68bfc8b2a02170e1855b08b2b74dea92899e075
szymonolk <olk.szymon@gmail.com>
2022-09-21 10:17:21 +0200

    add new component cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AddNewComponent} from "./add-new/add-new.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'addnew', component: AddNewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
