9e2c6659013b4d186a6fc739e0d8ad6a80c2d49b
szymonolk <olk.szymon@gmail.com>
2022-09-24 11:21:50 +0200

    Form in ts (not working at all :( )

--- src/app/add-new/add-new.component.ts
+++ src/app/add-new/add-new.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-add-new',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-new.component.css']
 })
 export class AddNewComponent implements OnInit {
+
+  newExpenseForm = new FormGroup({
+    expenseName: new FormControl(' '),
+    cost: new FormControl(' '),
+    priority: new FormControl(' '),
+    date: new FormControl(' ')
+  })
+
+  createNewExpense(){
+    console.log(this.newExpenseForm.value);
+  }
+
 
   constructor() { }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatSliderModule} from "@angular/material/slider";
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatNativeDateModule} from "@angular/material/core";
 import {MatFormFieldModule} from "@angular/material/form-field";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -33,7 +34,8 @@ import {MatFormFieldModule} from "@angular/material/form-field";
     MatSliderModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
